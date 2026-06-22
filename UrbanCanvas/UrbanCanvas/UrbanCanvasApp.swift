45fa6f319bf7d92138c5b00c56a522eabaf1d420
kays-dev <krys.b@outlook.com>
2026-06-22 11:34:15 +0200

    Project file added

---
+++ UrbanCanvas/UrbanCanvas/UrbanCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrbanCanvasApp.swift
+//  UrbanCanvas
+//
+//  Created by Apprenant174 on 22/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct UrbanCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
